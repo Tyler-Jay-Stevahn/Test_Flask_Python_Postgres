cc9a5d9b103b73c381a5ffea95f92d4436696b7c
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2020-02-16 05:42:56 -0600

    still broken

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,11 @@ db = SQLAlchemy()
 def create_app(config_type):
     # dev, test, prod
     app = Flask(__name__)
-    configuration = os.path.join(os.getcwd(), 'config', config_type, '.py')
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
     app.config.from_pyfile(configuration)
     db.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
+
     return app
