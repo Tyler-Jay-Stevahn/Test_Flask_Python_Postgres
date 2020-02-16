6d9cab3c72d82808c7a1431af462cb32c3d39563
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2020-02-16 02:05:11 -0600

    this is an update

--- Run.py
+++ Run.py
@@ -1,72 +1,75 @@
-# INCREMENTAL PYTHON CODE FOR CREATING BOOK TABLE
-# SECTION 5: LECTURE: 22
-
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, g
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config.update(
 
-    SECRET_KEY='topsecret',
+app.config.update(
+    SECRET_KEY='Stevahn23!!',
     SQLALCHEMY_DATABASE_URI='postgresql://postgres:Stevahn23!!@localhost/Medical_db',
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
 db = SQLAlchemy(app)
 
-# hello this is a change
 
+@app.before_request
+def some_function():
+    g.string = '<br> This code ran before any request'
+
+
+# BASIC ROUTE
 @app.route('/index')
 @app.route('/')
 def hello_flask():
-    return 'Hello Flask'
+    return 'Hello Flask! <br>' + g.string
 
 
+# QUERY STRINGS
 @app.route('/new/')
-def query_string(greeting='hello'):
+def query_strings(greeting='hello'):
     query_val = request.args.get('greeting', greeting)
-    return '<h1> the greeting is: {0} </h1>'.format(query_val)
+    return '<h1> the greeting is : {0} </h1>'.format(query_val) + g.string
 
 
+# REMOVE QUERY STRINGS
 @app.route('/user')
 @app.route('/user/<name>')
 def no_query_strings(name='mina'):
-    return '<h1> hello there ! {} </>'.format(name)
+    return '<h1> hello there ! {} </h1>'.format(name)
 
 
-# strings
+# STRINGS
 @app.route('/text/<string:name>')
 def working_with_strings(name):
     return '<h1> here is a string: ' + name + '</h1>'
 
 
-# numbers
+# NUMBERS
 @app.route('/numbers/<int:num>')
 def working_with_numbers(num):
     return '<h1> the number you picked is: ' + str(num) + '</h1>'
 
 
-# add numbers
+# MORE NUMBERS
 @app.route('/add/<int:num1>/<int:num2>')
 def adding_integers(num1, num2):
-    return '<h1> the sum is : {}'.format(num1 + num2) + '</h1>'
+    return '<h1>the sum is : {}'.format(num1 + num2) + '</h1>'
 
 
-# floats
+# FLOATS
 @app.route('/product/<float:num1>/<float:num2>')
 def product_two_numbers(num1, num2):
-    return '<h1> the product is : {}'.format(num1 * num2) + '</h1>'
+    return '<h1> the product is: {}'.format(num1 * num2) + '</h1>'
 
 
-# rendering templates
+# USING TEMPLATES
 @app.route('/temp')
 def using_templates():
     return render_template('hello.html')
 
 
-# JINJA TEMPLATES 1
+# JINJA TEMPLATES
 @app.route('/watch')
 def top_movies():
     movie_list = ['autopsy of jane doe',
@@ -81,7 +84,6 @@ def top_movies():
                            name='Harry')
 
 
-# JINJA TEMPLATES 2
 @app.route('/tables')
 def movies_plus():
     movies_dict = {'autopsy of jane doe': 02.14,
@@ -96,7 +98,6 @@ def movies_plus():
                            name='Sally')
 
 
-# JINJA2 - FILTERS
 @app.route('/filters')
 def filter_data():
     movies_dict = {'autopsy of jane doe': 02.14,
@@ -112,7 +113,6 @@ def filter_data():
                            film='a christmas carol')
 
 
-# JINJA2 - MACROS
 @app.route('/macros')
 def jinja_macros():
     movies_dict = {'autopsy of jane doe': 02.14,
@@ -122,25 +122,30 @@ def jinja_macros():
                    'john wick 2': 02.52,
                    'spiderman - homecoming': 1.48}
 
-    return render_template('using_macros.html', movies=movies_dict)
+    return render_template('using_macros.html',
+                           movies=movies_dict)
 
 
-# PUBLICATION TABLE
+@app.route('/session')
+def session_data():
+    if 'name' not in session:
+        session['name'] = 'harry'
+    return render_template('session.html', name=session['name'], session=session)
+
+
 class Publication(db.Model):
     __tablename__ = 'publication'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return 'The id is {}, Name is is {}'.format(self.id, self.name)
+        return 'Publisher is {}'.format(self.name)
 
 
-# BOOK TABLE
 class Book(db.Model):
     __tablename__ = 'book'
 
@@ -153,12 +158,10 @@ class Book(db.Model):
     num_pages = db.Column(db.Integer)
     pub_date = db.Column(db.DateTime, default=datetime.utcnow())
 
-    # ESTABLISH A RELATIONSHIP BETWEEN PUBLICATION AND BOOK TABLES
+    # Relationship
     pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
 
-    def __init__(self, title, author, avg_rating, book_format, image,
-                 num_pages, pub_id):
-
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
         self.title = title
         self.author = author
         self.avg_rating = avg_rating
